24a6f724542c134dec4c7bb745d920948b1226fd
aleks279 <szamora@pernix-solutions.com>
2015-02-09 20:14:09 -0600

    Added edit capybara tests

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-describe "create article process", :type => :feature do
-  it "creates a new article" do
+describe "create new article", :type => :feature do
+  it "successfully creates a new article" do
     visit '/articles/new'
     within('#new_article') do
       fill_in 'article_title', :with => 'This is an article'
@@ -12,15 +12,42 @@ describe "create article process", :type => :feature do
     expect(page).to have_content 'Articles'
   end
 
-  context "when successful" do
-    it "goes to the articles index" do
-
+  it "fails to create an article" do
+    visit '/articles/new'
+    within('#new_article') do
+      fill_in 'article_title', :with => ''
+      fill_in 'article_text', :with => 'This is the articles body'
     end
+
+    click_button 'Create Article'
+    expect(page).to have_content 'New Article'
   end
 
-  context "when fails" do
-    it "refresh the new page" do
+  context "edit existing article" do
+    let(:article) { FactoryGirl.create(:article) }
 
+    it "successfully updates an existing article" do
+      visit "articles/#{article.id}/edit"
+
+      within("#edit_article_#{article.id}") do
+        fill_in 'article_title', :with => 'New title'
+        fill_in 'article_text', :with => 'New body'
+      end
+
+      click_button 'Update Article'
+      expect(page).to have_content 'Title: New title'
+    end
+
+    it "fails to update an existing article" do
+      visit "articles/#{article.id}/edit"
+
+      within("#edit_article_#{article.id}") do
+        fill_in 'article_title', :with => ''
+        fill_in 'article_text', :with => 'New body'
+      end
+
+      click_button 'Update Article'
+      expect(page).to have_content 'Edit Article'
     end
   end
 end
